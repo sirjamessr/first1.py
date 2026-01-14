0e03f18a7d1b69c998397d77cd5294403ecc3182
sirjamessr <164093621+sirjamessr@users.noreply.github.com>
2026-01-14 17:13:27 -0600

    Fix typo in variable name from 'cant' to 'can't'

--- first2.py
+++ first2.py
@@ -2,7 +2,7 @@ print("Hello, World!")
 print('No love')
 her = ('Her Love means nothing to me')
 print(f'{her}')
-cant = ('I want nothing more to do with her and her horrilbe attitued')
+can't = ('I want nothing more to do with her and her horrible attitude')
 print(f'{cant}')
 print(f'{her}, {cant}')
 
