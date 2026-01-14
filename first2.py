33ea66a8dec3b96f6f6a57d4d9e45015e1c8eb67
sirjamessr <164093621+sirjamessr@users.noreply.github.com>
2026-01-14 10:27:29 -0600

    Add files via upload

---
+++ first2.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+print('No love')
+her = ('Her Love means nothing to me')
+print(f'{her}')
+cant = ('I want nothing more to do with her and her horrilbe attitued')
+print(f'{cant}')
+print(f'{her}, {cant}')
+
+
+print("Goodbye World!")
